50b0fa88da43042625ec3244f44ef0ffb05b2a86
Chance Rutledge <chancerutledge2@gmail.com>
2019-11-12 19:07:53 -0800

    MVP commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList}/>
-      <Route path="/movies/:id" component={Movie}/>
+      <Route path="/movies/:id" render={ (props)=> <Movie item={MovieList} {...props}/>}/>
+      {/* component={Movie}/> */}
       {/* <div>Replace this Div with your Routes</div> */}
     </div>
   );
